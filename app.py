ca4ff28be776213cdb9d8bc86fc23f3f35c35710
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-09-30 03:37:32 +0600

    cors fix again

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 import base64
 from io import BytesIO
 from matplotlib.figure import Figure
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+CORS(app)
 
 allData = {
     "D03":{
@@ -109,7 +109,6 @@ def graphFunction(functionType, inputValue):
         return int(inputValue) ** 3
 
 @app.route("/getGraph", methods = ["POST"])
-@cross_origin(supports_credentials=True)
 def getGraph():
     requestBody = request.get_json()
     print(f'requestBody is {requestBody}')
